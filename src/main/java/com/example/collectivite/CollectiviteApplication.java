d83090af77aad6201243f2124a4057b98e2daed0
nyamyjese <hei.jese.2@gmail.com>
2026-04-16 14:42:15 +0100

    initial commit

---
+++ src/main/java/com/example/collectivite/CollectiviteApplication.java
@@ -0,0 +1,13 @@
+package com.example.collectivite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectiviteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectiviteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/collectivite/CollectiviteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collectivite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectiviteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
