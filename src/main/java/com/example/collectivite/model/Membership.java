e6979b32440479649cd57a68348c52e7236b1bd7
nyamyjese <hei.jese.2@gmail.com>
2026-04-21 23:27:36 +0100

    feat:add Membership file in model package for Member

---
+++ src/main/java/com/example/collectivite/model/Membership.java
@@ -0,0 +1,81 @@
+package com.example.collectivite.model;
+
+import java.time.LocalDate;
+
+public class Membership {
+    private Integer id ;
+    private Integer memberId;
+    private Integer collectivityId;
+    private Poste poste;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Membership() {
+    }
+
+    public Membership(Integer id,
+                      Integer memberId,
+                      Integer collectivityId,
+                      Poste poste,
+                      LocalDate startDate,
+                      LocalDate endDate) {
+        this.id = id;
+        this.memberId = memberId;
+        this.collectivityId = collectivityId;
+        this.poste = poste;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public boolean isActive() {
+        return endDate ==  null;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Integer memberId) {
+        this.memberId = memberId;
+    }
+
+    public Integer getCollectivityId() {
+        return collectivityId;
+    }
+
+    public void setCollectivityId(Integer collectivityId) {
+        this.collectivityId = collectivityId;
+    }
+
+    public Poste getPoste() {
+        return poste;
+    }
+
+    public void setPoste(Poste poste) {
+        this.poste = poste;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
