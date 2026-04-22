97c88560a0215c12bd60cdb60159608f209cb849
nelio-gio <hei.rocques@gmail.com>
2026-04-22 18:08:27 +0300

    change code in CreateContributionRequest

--- src/main/java/com/example/collectivite/dto/CreateContributionRequest.java
+++ src/main/java/com/example/collectivite/dto/CreateContributionRequest.java
@@ -1,32 +1,20 @@
 package com.example.collectivite.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class CreateContributionRequest {
-    @NotBlank(message = "Member ID is required")
+
     private String memberId;
-
-    @NotBlank(message = "Type is required")
     private String type;
-
-    @Min(value = 1, message = "Amount must be positive")
     private int amount;
-
-    @NotBlank(message = "Payment mode is required")
     private String paymentMode;
-
     private String transactionReference;
-
-    @NotNull(message = "Collection date is required")
     private LocalDate collectionDate;
+
 
     public String getMemberId() {
         return memberId;
     }
-
     public void setMemberId(String memberId) {
         this.memberId = memberId;
     }
@@ -34,7 +22,6 @@ public class CreateContributionRequest {
     public String getType() {
         return type;
     }
-
     public void setType(String type) {
         this.type = type;
     }
@@ -42,7 +29,6 @@ public class CreateContributionRequest {
     public int getAmount() {
         return amount;
     }
-
     public void setAmount(int amount) {
         this.amount = amount;
     }
@@ -50,7 +36,6 @@ public class CreateContributionRequest {
     public String getPaymentMode() {
         return paymentMode;
     }
-
     public void setPaymentMode(String paymentMode) {
         this.paymentMode = paymentMode;
     }
@@ -58,7 +43,6 @@ public class CreateContributionRequest {
     public String getTransactionReference() {
         return transactionReference;
     }
-
     public void setTransactionReference(String transactionReference) {
         this.transactionReference = transactionReference;
     }
@@ -66,7 +50,6 @@ public class CreateContributionRequest {
     public LocalDate getCollectionDate() {
         return collectionDate;
     }
-
     public void setCollectionDate(LocalDate collectionDate) {
         this.collectionDate = collectionDate;
     }
