57c6c1c2f8a258cc17e12fa0722a7a48708e14a7
nyamyjese <hei.jese.2@gmail.com>
2026-05-06 23:50:24 +0100

    fix(controller): resolve a bug in MemberController

--- src/main/java/com/example/collectivite/controller/MemberController.java
+++ src/main/java/com/example/collectivite/controller/MemberController.java
@@ -1,9 +1,13 @@
 package com.example.collectivite.controller;
 
+import com.example.collectivite.config.DBConnection;
 import com.example.collectivite.dto.AdmitMemberRequest;
 import com.example.collectivite.dto.MemberResponse;
 import com.example.collectivite.exception.AdmissionException;
+import com.example.collectivite.repository.*;
 import com.example.collectivite.service.MemberAdmissionService;
+import com.example.collectivite.validator.AdmissionValidator;
+
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +19,27 @@ public class MemberController {
 
     private final MemberAdmissionService admissionService;
 
-    public MemberController(MemberAdmissionService admissionService) {
-        this.admissionService = admissionService;
+    public MemberController() {
+        DBConnection db = DBConnection.getInstance();
+
+        MemberRepository      memberRepo      = new MemberRepository(db);
+        CollectivityRepository collectivityRepo = new CollectivityRepository(db);
+        MembershipRepository  membershipRepo  = new MembershipRepository(db);
+        SponsorshipRepository sponsorshipRepo  = new SponsorshipRepository(db);
+        PaymentRepository     paymentRepo     = new PaymentRepository(db);
+
+        AdmissionValidator admissionValidator = new AdmissionValidator(
+                memberRepo, membershipRepo, collectivityRepo);
+
+        this.admissionService = new MemberAdmissionService(
+                memberRepo,
+                sponsorshipRepo,
+                membershipRepo,
+                paymentRepo,
+                collectivityRepo,
+                db,
+                admissionValidator
+        );
     }
 
     @PostMapping
